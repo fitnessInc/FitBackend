7c53aaa4e1d6bc04418c982efc1e0959b87453a1
Adnane <nanitosse@gmail.com>
2025-01-16 21:13:59 -0500

    firebase-Admin-Initialisation

---
+++ firebase/firebaseConfig.js
@@ -0,0 +1,16 @@
+ const admin  = require('firebase-admin');
+const serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket:'sporty-6fdac.appspot.com'
+
+
+});
+
+
+
+const db = admin.firestore();
+const bucket = admin.storage().bucket();
+
+module.exports = {admin,db,bucket}
